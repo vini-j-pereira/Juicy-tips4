6f4cbe1111f55cb26966c82cf78ba888003d3e50
vini-j-pereira <viniciusjosepereira@gmail.com>
2025-03-15 17:09:09 -0300

    commit script btn 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 document.getElementById("btnTelegram").addEventListener("click", function() {
     window.location.href = "https://t.me/+YTwM7kqwdn5jZmRh";
 });
+
+document.getElementById("btnTelegram2").addEventListener("click", function() {
+    window.location.href = "https://t.me/+YTwM7kqwdn5jZmRh";
+});
